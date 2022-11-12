8cc6bc53389716eba00625926fe99a8f965fe583
Nusret Bilallaj <netktit@gmail.com>
2022-11-12 16:31:58 +0100

    Add project files.

---
+++ Group3Web2/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Group3Web2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group3Web2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Studenti stud1 = new Studenti("Kastriot", "Gjojka");
+
+            return View(stud1);
+        }
+        public IActionResult Index1()
+        {
+            return View();
+        }
+
+        public IActionResult MesoHtml()
+        {
+            return View();
+        }
+
+        public IActionResult ParaqitListen()
+        {
+            List<Studenti> studentet = new List<Studenti>();
+            Studenti stud1 = new Studenti("Blend", "Krasniqi");
+            Studenti stud2 = new Studenti("Arbresha", "Alickaj");
+            Studenti stud3 = new Studenti("Kastriot", "Gjoka");
+            var stud4 = new Studenti("A", "B");
+
+            studentet.Add(stud1);
+            studentet.Add(stud2);
+            studentet.Add(stud3);
+            studentet.Add( new Studenti("X","Y"));
+            studentet.Add(stud4);
+
+            return View(studentet);
+        }
+    }
+}
---
+++ Group3Web2/Models/Studenti.cs
@@ -0,0 +1,19 @@
+﻿namespace Group3Web2.Models
+{
+    public class Studenti
+    {
+        public Studenti(string emri,string mbiemri)
+        {
+            Emri=emri;
+            Mbiemri=mbiemri;
+        }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+
+        public string SayHello()
+        {
+            return $"Hello student {Emri} {Mbiemri} ";
+        }
+
+    }
+}
